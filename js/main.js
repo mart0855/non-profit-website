32bd1ba59d50177d083378e5044f679fddd8d859
Mark Collantes <collantes.mark.mgc@gmail.com>
2015-03-09 07:22:24 -0400

    feat(html, css, js): add register toggle

--- js/main.js
+++ js/main.js
@@ -26,6 +26,9 @@ var passWrd = $('.password');
 var lrWrppr = $('.log-reg-wrapper');
 var exitLr = $('.exit-lr');
 var lrLogIn = $('.lr-log-in');
+var regTggle = $('.register-toggle');
+var regWrppr = $('.register-wrapper');
+var regExit = $('.register-exit');
 
 logIn.click(function(e) {
 	e.preventDefault();
@@ -54,6 +57,28 @@ lrLogIn.click(function(e){
 		siteWrapper.addClass('no-scroll');
 	}
 });
+
+regTggle.click(function(e){
+	e.preventDefault();
+	var isActive = regWrppr.attr('data-state');
+		if (isActive == 'expand') {
+			regWrppr.attr('data-state', 'collapse');
+			siteWrapper.removeClass('no-scroll');
+	} else {
+			regWrppr.attr('data-state', 'expand');
+			topNav.attr('data-state','collapse');
+			siteWrapper.addClass('no-scroll');
+	}
+	lrWrppr.attr('data-state', 'collapse');
+	hamburgerToggle.removeClass('expand');
+});
+
+regExit.click(function(e){
+	e.preventDefault();
+	regWrppr.attr('data-state', 'collapse');
+	hamburgerToggle.removeClass('expand');
+});
+
 
 exitLr.click(function(e){
 	e.preventDefault();
